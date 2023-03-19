08ca68219e698970377920d4369486a7d82af1f2
fuderiki <frederic.seraphine@gmail.com>
2023-03-19 19:37:39 +0900

    Update astar.rs

--- src/cpu/astar.rs
+++ src/cpu/astar.rs
@@ -1,1 +1,5 @@
-
+pub fn astar_cost_function(current: &Node, goal: &Node, g: i32, h: fn(&Node, &Node) -> i32) -> i32 {
+    let current_cost = g;
+    let heuristic_cost = h(current, goal); //Distance Function
+    actual_cost + heuristic_cost
+}
