d60875bc364e3606b7ea5f979f898f20a5e3498b
fuderiki <frederic.seraphine@gmail.com>
2023-03-19 19:47:37 +0900

    Update distance.rs

--- src/cpu/distance.rs
+++ src/cpu/distance.rs
@@ -1,11 +1,11 @@
-fn manhattan_distance(current: &Node, goal: &Node) -> f64 {
-    let dx = (current.x - goal.x).abs();
-    let dy = (current.y - goal.y).abs();
+fn manhattan_distance(a: &Node, b: &Node) -> f64 {
+    let dx = (a.x - b.x).abs();
+    let dy = (a.y - b.y).abs();
     dx + dy
 }
 
-fn euclidean_distance(current: &Node, goal: &Node) -> f64 {
-    let dx = (current.x - goal.x) as f64;
-    let dy = (current.y - goal.y) as f64;
+fn euclidean_distance(a: &Node, b: &Node) -> f64 {
+    let dx = (a.x - b.x) as f64;
+    let dy = (a.y - b.y) as f64;
     (dx.powi(2) + dy.powi(2)).sqrt()
 }
